e717b04864dc7f657a98ed5387d17fbb34ab0bb5
Fx4Nim4Tor <mega123foxy123@gmail.com>
2026-03-10 09:37:57 -0300

    criação de produtos mais produtos em destaque pronto

--- gomesmoveis/core/models.py
+++ gomesmoveis/core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=200)
+    descricao = models.TextField()
+    imagem = models.ImageField(upload_to="produtos/")
+    destaque = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.nome
--- gomesmoveis/core/views.py
+++ gomesmoveis/core/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from .models import Produto
 
 # Create your views here.
 def inicio(request):
-    return render(request,"index.html")
+    produtos_destaque = Produto.objects.filter(destaque=True)
+
+    return render(request, "index.html", {
+        "produtos_destaque": produtos_destaque
+    })
+    # return render(request,"index.html")
+
+def adm(request):
+    return render(request, "admin/admin.html")
