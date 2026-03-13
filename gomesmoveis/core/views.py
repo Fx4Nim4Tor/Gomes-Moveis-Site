f2da2ab864d727565560e5f1f87bfb56e68de7c3
Matheus <mega123foxy123@gmail.com>
2026-03-12 21:27:13 -0300

    puxando produtos relacionados na pagina do produto.
    puxa de forma aleatorioa baseada na categoria mas puxa so 5

--- gomesmoveis/core/views.py
+++ gomesmoveis/core/views.py
@@ -13,8 +13,16 @@ def inicio(request):
 def produto(request, slug):
     produto = get_object_or_404(Produto, slug=slug)
 
+    produtos_relacionados = (
+        Produto.objects
+        .filter(categoria=produto.categoria)   # mesma categoria
+        .exclude(id=produto.id)                # remove o produto atual
+        .order_by("?")[:5]                     # aleatório e limitado a 5
+    )
+
     return render(request, "produto.html", {
-        "produto": produto
+        "produto": produto,
+        "produtos_relacionados": produtos_relacionados
     })
 
 
