80a836da30d7d11e7a43201c3854ccba1ee569a5
Matheus <mega123foxy123@gmail.com>
2026-03-26 21:26:13 -0300

    alterando principalmente design

---
+++ gomesmoveis/core/static/js/adm/categorias_adm.js
@@ -0,0 +1,7 @@
+document.getElementsByClassName("btn-edit-adm")[0].addEventListener("click", function(){
+    window.location.href = this.dataset.url;
+});
+document.getElementsByClassName("btn-delet-adm")[0].addEventListener("click", function(){
+    window.location.href = this.dataset.url;
+});
+
