70751062706bf7f16906326f9ae7603b2d52fff4
Fx4Nim4Tor <mega123foxy123@gmail.com>
2026-04-16 17:56:44 -0300

    teste wsgi.py

--- gomesmoveis/gomesmoveis/wsgi.py
+++ gomesmoveis/gomesmoveis/wsgi.py
@@ -1,13 +1,27 @@
-"""
-WSGI config for gomesmoveis project.
+# """
+# WSGI config for gomesmoveis project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+# It exposes the WSGI callable as a module-level variable named ``application``.
 
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
+# For more information on this file, see
+# https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
+# """
+
+# import os
+
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gomesmoveis.settings')
+
+# application = get_wsgi_application()
 
 import os
+import sys
+from pathlib import Path
+
+# adiciona a pasta do projeto no path
+BASE_DIR = Path(__file__).resolve().parent.parent
+sys.path.append(str(BASE_DIR))
 
 from django.core.wsgi import get_wsgi_application
 
