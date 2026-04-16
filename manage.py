9a9225fa1a14f8a9b313c8f0674a406950e5707d
Fx4Nim4Tor <mega123foxy123@gmail.com>
2026-04-16 17:52:39 -0300

    alteração menage para deploy

--- manage.py
+++ manage.py
@@ -1,5 +1,10 @@
 import os
 import sys
+from pathlib import Path
+
+# adiciona a pasta gomesmoveis no path
+BASE_DIR = Path(__file__).resolve().parent
+sys.path.append(str(BASE_DIR / "gomesmoveis"))
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gomesmoveis.settings")
