9668824e6d8f9d10b91037c6b63bf3ac55297c06
Fx4Nim4Tor <mega123foxy123@gmail.com>
2026-04-17 14:40:27 -0300

    Teste deploy

--- gomesmoveis/gomesmoveis/wsgi.py
+++ gomesmoveis/gomesmoveis/wsgi.py
@@ -28,3 +28,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gomesmoveis.settings')
 
 application = get_wsgi_application()
+
+# Vercel precisa dessa variável
+app = application
